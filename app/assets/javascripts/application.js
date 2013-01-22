4e8b9b68f19ba805b2dd112d3c50fec8243f3dfe
jinny <jinny@jinny-Vostro-3300.(none)>
2013-01-22 17:34:23 +0900

    project init

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,25 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// GO AFTER THE REQUIRES BELOW.
+//
+//= require jquery
+//= require jquery_ujs
+//= require json2.js
+//= require underscore-min
+//= require backbone
+// require backbone.localStorage
+// require backbone-localstorage
+//= require handlebars-1.0.rc.1.min
+//= require handlebars-helpers
+//require jquery.ba-hashchange
+//= require jquery.jsonp-2.4.0.min
+//= require todos
+//= require_tree .
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todoapp::Application.config.secret_token = '674f5ba8aab92b7d5edb97670be8c114857c04fe894835b591beb9c3bef99d577392f7caf8b1b46a0e0190d02e9b92edaa4ed7a9f7295bc40f778ba854099457'
